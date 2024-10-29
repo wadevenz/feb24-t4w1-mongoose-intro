2b0fc0b2ff22ac30e0addca1b9be80b9ad89a4cb
Wade <158378370+wadevenz@users.noreply.github.com>
2024-10-29 19:39:19 +1000

    feat: post model implemented

---
+++ src/models/PostModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+// 1. Make a schema
+
+// const someSchema = new mongoose.Schema(objectDefiningDataRules, SchemaOptions);
+const PostSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minLength: 4,
+        trim: true
+    },
+    content: String,
+    date: {
+        type: Date,
+        default: Date.now // Mongoose will run Date.now() every time will make a doc
+        // default: new Date(Date.now()) // Mongoose will run Date.now once when the app loads the file
+    },
+});
+
+// 2. Make a model based on the schema
+const PostModel = mongoose.model("Post", PostSchema);
+
+// 3. Export the model for the rest of our code to use
+module.exports = {
+    PostModel
+}
