01de10ac844ea3a5f2d671e1ebcf06349cd17d5c
Wade <158378370+wadevenz@users.noreply.github.com>
2024-10-31 17:34:37 +1000

    feat: connec to mongodb

--- src/index.js
+++ src/index.js
@@ -4,15 +4,24 @@
 // - Get the port
 // - Tell the server to listen to web traffic
 
+require("dotenv").config();
+
 // Server is configured in this file, not in index.js:
 const {app} = require("./server.js");
+const { dbConnect } = require("./utils/database.js");
 
 // Get the PORT value from envirnment variables
 const PORT = process.env.PORT || 8080
 
+// dbConnect().then(() => {
+//     app.listen();
+// })
 
 // app.listen(port, callback)
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+
+    await dbConnect();
+
     // Server is runnning if we reach this point in the code.
     console.log("Server is running on port " + PORT);
 });
---
+++ src/utils/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { PostModel } = require("../models/PostModel");
+
+
+
+// Function to connect to whatever DB our environment variable says to connect to
+async function dbConnect(){
+    console.log(process.env.DATABASE_URL);
+    let databaseUrl = process.env.DATABASE_URL || `mongodb://127.0.0.1:27017/${process.env.npm_package_name}`;
+
+    await mongoose.connect(databaseUrl);
+};
+
+
+module.exports = {
+    dbConnect
+}
